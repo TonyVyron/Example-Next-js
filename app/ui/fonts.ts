ac9f0a50821dc45883f253461c7db78f618c455c
TonyVyron <105890105+TonyVyron@users.noreply.github.com>
2024-10-03 09:41:05 -0600

    fix deploy

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Montserrat, Lusitana} from 'next/font/google'
+
+export const montserrat = Montserrat({subsets:['latin'], weight:'300' })
+
+export const lusitana = Lusitana({subsets:['latin'], weight:'400' })
